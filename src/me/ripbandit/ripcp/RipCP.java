061ae411e065149a93d42520b76cc5f4be808173
Ripbandit <ripbandityt@gmail.com>
2016-11-05 19:59:36 -0400

    Initial plugin creation

---
+++ src/me/ripbandit/ripcp/RipCP.java
@@ -0,0 +1,18 @@
+package me.ripbandit.ripcp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by rip on 11/5/16.
+ */
+public class RipCP extends JavaPlugin{
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
