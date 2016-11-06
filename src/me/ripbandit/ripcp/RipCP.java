2a532913b29da6ceebdaf3879c54b5d6d184d7a2
Innectic <innectic@gmail.com>
2016-11-05 17:24:51 -0700

    Create config method, add instance

--- src/me/ripbandit/ripcp/RipCP.java
+++ src/me/ripbandit/ripcp/RipCP.java
@@ -2,17 +2,40 @@ package me.ripbandit.ripcp;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 /**
  * Created by rip on 11/5/16.
  */
-public class RipCP extends JavaPlugin{
+public class RipCP extends JavaPlugin {
+
+    public static RipCP instance;
+
     @Override
     public void onEnable() {
-
+        instance = this;
+        loadConfig();
     }
 
     @Override
     public void onDisable() {
+        instance = null;
+    }
 
+    private void loadConfig() {
+        try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+            File file = new File(getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                getLogger().info("Config found, loading.");
+                saveDefaultConfig();
+            } else {
+                getLogger().info("Config not found, creating!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
